26e5d7c3c1fd8dee671a17c20387d9ef55282c82
fjoost-ubuntu <fjoostr@gmail.com>
2019-05-30 02:10:08 -0400

    añade script bd v4[eng]
    se pasan todos los atributos y nombres de tablas a ingles. se ajustan
    las columnas de la tabla USER para que coincidan con las variables que
    solicita auth de laravel

--- resources/script_mysql/script_bd.sql
+++ resources/script_mysql/script_bd.sql
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `AUTO`
 --
 
-CREATE TABLE `AUTO` (
-  `pat_auto` varchar(6) NOT NULL,
-  `id_mod` int(11) NOT NULL,
-  `id_marca` int(11) NOT NULL
+CREATE TABLE `CAR` (
+  `id_car` varchar(6) NOT NULL,
+  `id_model` int(11) NOT NULL,
+  `id_brand` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -40,9 +40,9 @@ CREATE TABLE `AUTO` (
 -- Table structure for table `CODIGO_QR`
 --
 
-CREATE TABLE `CODIGO_QR` (
-  `id_codqr` int(11) NOT NULL,
-  `cont_qr` tinyint(1) NOT NULL
+CREATE TABLE `QR_CODE` (
+  `id_qrcode` int(11) NOT NULL,
+  `count_qrcode` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -51,12 +51,12 @@ CREATE TABLE `CODIGO_QR` (
 -- Table structure for table `DETALLE_SERVICIO`
 --
 
-CREATE TABLE `DETALLE_SERVICIO` (
-  `id_detserv` int(11) NOT NULL,
-  `horainicio_detserv` time NOT NULL,
-  `horatermino_detser` time NOT NULL,
-  `duracion_detser` int(11) NOT NULL,
-  `valor_detserv` varchar(45) NOT NULL
+CREATE TABLE `SERVICE_DETAIL` (
+  `id_servdet` int(11) NOT NULL,
+  `starttime_servdet` time NOT NULL,
+  `stoptime_servdet` time NOT NULL,
+  `duration_servdet` int(11) NOT NULL,
+  `mount_servdet` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -65,9 +65,9 @@ CREATE TABLE `DETALLE_SERVICIO` (
 -- Table structure for table `MARCA`
 --
 
-CREATE TABLE `MARCA` (
-  `id_marca` int(11) NOT NULL,
-  `nom_marca` varchar(45) NOT NULL
+CREATE TABLE `BRAND` (
+  `id_brand` int(11) NOT NULL,
+  `name_brand` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -76,10 +76,10 @@ CREATE TABLE `MARCA` (
 -- Table structure for table `MODELO_TIPO_AUTO`
 --
 
-CREATE TABLE `MODELO_TIPO_AUTO` (
-  `id_mod` int(11) NOT NULL,
-  `nom_mod` varchar(45) NOT NULL,
-  `tipo_mod` varchar(45) NOT NULL
+CREATE TABLE `MODEL` (
+  `id_model` int(11) NOT NULL,
+  `name_model` varchar(45) NOT NULL,
+  `type_model` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -88,10 +88,10 @@ CREATE TABLE `MODELO_TIPO_AUTO` (
 -- Table structure for table `PAGO`
 --
 
-CREATE TABLE `PAGO` (
-  `id_pago` int(11) NOT NULL,
-  `nom_pago` varchar(45) NOT NULL,
-  `id_tipopago` int(11) NOT NULL
+CREATE TABLE `PAYMENT` (
+  `id_payment` int(11) NOT NULL,
+  `name_payment` varchar(45) NOT NULL,
+  `id_typepayment` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -100,11 +100,11 @@ CREATE TABLE `PAGO` (
 -- Table structure for table `PLAZA`
 --
 
-CREATE TABLE `PLAZA` (
-  `id_plaza` int(11) NOT NULL,
-  `num_plaza` int(11) NOT NULL,
-  `est_plaza` tinyint(1) NOT NULL,
-  `id_seccionplaza` int(11) NOT NULL
+CREATE TABLE `SEAT` (
+  `id_seat` int(11) NOT NULL,
+  `numb_seat` int(11) NOT NULL,
+  `state_seat` tinyint(1) NOT NULL,
+  `id_seatsection` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -113,17 +113,17 @@ CREATE TABLE `PLAZA` (
 -- Table structure for table `RESERVA`
 --
 
-CREATE TABLE `RESERVA` (
-  `id_reserv` int(11) NOT NULL,
-  `fecha_reserv` date NOT NULL,
-  `hora_reserv` time DEFAULT NULL,
-  `id_tarif` int(11) NOT NULL,
-  `rut_usu` int(11) NOT NULL,
-  `id_pago` int(11) NOT NULL,
-  `id_tiporeserv` int(11) NOT NULL,
-  `id_detserv` int(11) NOT NULL,
-  `id_codqr` int(11) NOT NULL,
-  `id_plaza` int(11) NOT NULL
+CREATE TABLE `RESERVE` (
+  `id_reserve` int(11) NOT NULL,
+  `date_reserve` date NOT NULL,
+  `hour_reserve` time DEFAULT NULL,
+  `id_tariff` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_payment` int(11) NOT NULL,
+  `id_reservetype` int(11) NOT NULL,
+  `id_servdet` int(11) NOT NULL,
+  `id_qrcode` int(11) NOT NULL,
+  `id_seat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -132,9 +132,9 @@ CREATE TABLE `RESERVA` (
 -- Table structure for table `SECCION_PLAZA`
 --
 
-CREATE TABLE `SECCION_PLAZA` (
-  `id_seccionplaza` int(11) NOT NULL,
-  `nom_seccionplaza` int(11) NOT NULL
+CREATE TABLE `SEAT_SECTION` (
+  `id_seatsection` int(11) NOT NULL,
+  `name_seatsection` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -143,11 +143,11 @@ CREATE TABLE `SECCION_PLAZA` (
 -- Table structure for table `TARIFA`
 --
 
-CREATE TABLE `TARIFA` (
-  `id_tarif` int(11) NOT NULL,
-  `nom_tarif` varchar(45) NOT NULL,
-  `descr_tarif` varchar(45) NOT NULL,
-  `valor_tarif` int(11) NOT NULL
+CREATE TABLE `TARIFF` (
+  `id_tariff` int(11) NOT NULL,
+  `name_tariff` varchar(45) NOT NULL,
+  `description_tariff` varchar(45) NOT NULL,
+  `value_tariff` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -156,10 +156,10 @@ CREATE TABLE `TARIFA` (
 -- Table structure for table `TIPO_PAGO`
 --
 
-CREATE TABLE `TIPO_PAGO` (
-  `id_tipopago` int(11) NOT NULL,
-  `nom_tipopago` varchar(45) NOT NULL,
-  `det_tipopago` varchar(45) NOT NULL
+CREATE TABLE `PAYMENT_TYPE` (
+  `id_paytype` int(11) NOT NULL,
+  `name_paytype` varchar(45) NOT NULL,
+  `detail_paytype` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -168,10 +168,10 @@ CREATE TABLE `TIPO_PAGO` (
 -- Table structure for table `TIPO_RESERVA`
 --
 
-CREATE TABLE `TIPO_RESERVA` (
-  `id_tiporeserv` int(11) NOT NULL,
-  `nom_tiporeserv` varchar(45) NOT NULL,
-  `descr_tiporeserv` varchar(45) NOT NULL
+CREATE TABLE `RESERVE_TYPE` (
+  `id_reservetype` int(11) NOT NULL,
+  `name_reservetype` varchar(45) NOT NULL,
+  `description_reservetype` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='	';
 
 -- --------------------------------------------------------
@@ -180,24 +180,24 @@ CREATE TABLE `TIPO_RESERVA` (
 -- Table structure for table `USUARIO`
 --
 
-CREATE TABLE `USUARIO` (
-  `rut_usu` int(11) NOT NULL,
-  `nom_usu` varchar(45) NOT NULL,
-  `ape_usu` varchar(45) NOT NULL,
-  `correo_usu` varchar(45) NOT NULL,
-  `contr_usu` varchar(60) NOT NULL,
-  `telefono_usu` int(11) NOT NULL,
-  `nacimiento_usu` date NOT NULL,
+CREATE TABLE `USER` (
+  `id_user` int(11) NOT NULL, /*rut*/
+  `name_user` varchar(45) NOT NULL,
+  `lstname_user` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `borndate_user` date NOT NULL,
   `updated_at` datetime NOT NULL,
   `created_at` datetime NOT NULL,
-  `id_tipousu` int(11) NOT NULL DEFAULT '1'
+  `id_usertype` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `USUARIO`
 --
 
-INSERT INTO `USUARIO` (`rut_usu`, `nom_usu`, `ape_usu`, `correo_usu`, `contr_usu`, `telefono_usu`, `nacimiento_usu`, `updated_at`, `created_at`, `id_tipousu`) VALUES
+INSERT INTO `USUARIO` (`id_user`, `name_user`, `lstnane_user`, `email`, `password`, `phone_user`, `borndate_user`, `updated_at`, `created_at`, `id_typeuser`) VALUES
 (19461893, 'Fernando', 'Joost', 'fjoostr@gmail.com', '$2y$10$.Hn5nHNnCIMPsfhhaH3YWOB5kbriVk3chZICwsMdLodLe5DbABHhK', 944175966, '1997-04-07', '2019-05-24 07:15:56', '2019-05-24 07:15:56', 1);
 
 -- --------------------------------------------------------
@@ -206,10 +206,10 @@ INSERT INTO `USUARIO` (`rut_usu`, `nom_usu`, `ape_usu`, `correo_usu`, `contr_usu
 -- Table structure for table `USUARIO_AUTO`
 --
 
-CREATE TABLE `USUARIO_AUTO` (
-  `id_autousu` int(11) NOT NULL,
-  `rut_usu` int(11) NOT NULL,
-  `pat_auto` varchar(6) NOT NULL
+CREATE TABLE `USER_CAR` (
+  `id_usercar` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_car` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -219,111 +219,111 @@ CREATE TABLE `USUARIO_AUTO` (
 --
 -- Indexes for table `AUTO`
 --
-ALTER TABLE `AUTO`
-  ADD PRIMARY KEY (`pat_auto`),
-  ADD UNIQUE KEY `patenteVehiculo_UNIQUE` (`pat_auto`),
-  ADD KEY `fk_id_mod` (`id_mod`),
-  ADD KEY `fk_id_marca` (`id_marca`);
+ALTER TABLE `CAR`
+  ADD PRIMARY KEY (`id_car`),
+  ADD UNIQUE KEY `patenteVehiculo_UNIQUE` (`id_car`),
+  ADD KEY `fk_id_model` (`id_model`),
+  ADD KEY `fk_id_brand` (`id_brand`);
 
 --
 -- Indexes for table `CODIGO_QR`
 --
-ALTER TABLE `CODIGO_QR`
-  ADD PRIMARY KEY (`id_codqr`);
+ALTER TABLE `QR_CODE`
+  ADD PRIMARY KEY (`id_qrcode`);
 
 --
 -- Indexes for table `DETALLE_SERVICIO`
 --
-ALTER TABLE `DETALLE_SERVICIO`
-  ADD PRIMARY KEY (`id_detserv`),
-  ADD UNIQUE KEY `id_detserv_UNIQUE` (`id_detserv`);
+ALTER TABLE `SERVICE_DETAIL`
+  ADD PRIMARY KEY (`id_servdet`),
+  ADD UNIQUE KEY `id_servdet_UNIQUE` (`id_servdet`);
 
 --
 -- Indexes for table `MARCA`
 --
-ALTER TABLE `MARCA`
-  ADD PRIMARY KEY (`id_marca`),
-  ADD UNIQUE KEY `id_marca_UNIQUE` (`id_marca`);
+ALTER TABLE `BRAND`
+  ADD PRIMARY KEY (`id_brand`),
+  ADD UNIQUE KEY `id_brand_UNIQUE` (`id_brand`);
 
 --
 -- Indexes for table `MODELO_TIPO_AUTO`
 --
-ALTER TABLE `MODELO_TIPO_AUTO`
-  ADD PRIMARY KEY (`id_mod`),
-  ADD UNIQUE KEY `id_modelo_UNIQUE` (`id_mod`);
+ALTER TABLE `MODEL`
+  ADD PRIMARY KEY (`id_model`),
+  ADD UNIQUE KEY `id_model_UNIQUE` (`id_model`);
 
 --
 -- Indexes for table `PAGO`
 --
-ALTER TABLE `PAGO`
-  ADD PRIMARY KEY (`id_pago`),
-  ADD UNIQUE KEY `id_pago_UNIQUE` (`id_pago`),
-  ADD KEY `fk_id_tipopago` (`id_tipopago`);
+ALTER TABLE `PAYMENT`
+  ADD PRIMARY KEY (`id_payment`),
+  ADD UNIQUE KEY `id_payment_UNIQUE` (`id_payment`),
+  ADD KEY `fk_id_paytype` (`id_paytype`);
 
 --
 -- Indexes for table `PLAZA`
 --
-ALTER TABLE `PLAZA`
-  ADD PRIMARY KEY (`id_plaza`),
-  ADD UNIQUE KEY `id_plaza_UNIQUE` (`id_plaza`),
-  ADD KEY `fk_id_seccionplaza_idx` (`id_seccionplaza`);
+ALTER TABLE `SEAT`
+  ADD PRIMARY KEY (`id_seat`),
+  ADD UNIQUE KEY `id_seat_UNIQUE` (`id_seat`),
+  ADD KEY `fk_id_seatsection` (`id_seatsection`);
 
 --
 -- Indexes for table `RESERVA`
 --
-ALTER TABLE `RESERVA`
-  ADD PRIMARY KEY (`id_reserv`),
-  ADD UNIQUE KEY `id_RESERVA_UNIQUE` (`id_reserv`),
-  ADD KEY `fk_id_tarif` (`id_tarif`),
-  ADD KEY `fk_rut_usu` (`rut_usu`),
-  ADD KEY `fk_id_pago` (`id_pago`),
-  ADD KEY `fk_id_tiporeserv` (`id_tiporeserv`),
-  ADD KEY `fk_id_detserv` (`id_detserv`),
-  ADD KEY `fk_id_codqr` (`id_codqr`),
-  ADD KEY `fk_id_plaza` (`id_plaza`);
+ALTER TABLE `RESERVE`
+  ADD PRIMARY KEY (`id_reserve`),
+  ADD UNIQUE KEY `id_reserve_UNIQUE` (`id_reserve`),
+  ADD KEY `fk_id_tariff` (`id_tariff`),
+  ADD KEY `fk_id_user` (`id_user`),
+  ADD KEY `fk_id_payment` (`id_payment`),
+  ADD KEY `fk_id_tiporeserv` (`id_reservetype`),
+  ADD KEY `fk_id_servdet` (`id_servdet`),
+  ADD KEY `fk_id_qrcode` (`id_qrcode`),
+  ADD KEY `fk_id_seat` (`id_seat`);
 
 --
 -- Indexes for table `SECCION_PLAZA`
 --
-ALTER TABLE `SECCION_PLAZA`
-  ADD PRIMARY KEY (`id_seccionplaza`),
-  ADD UNIQUE KEY `id_plaza_UNIQUE` (`id_seccionplaza`);
+ALTER TABLE `SEAT_SECTION`
+  ADD PRIMARY KEY (`id_seatsection`),
+  ADD UNIQUE KEY `id_seat_section_UNIQUE` (`id_seatsection`);
 
 --
 -- Indexes for table `TARIFA`
 --
-ALTER TABLE `TARIFA`
-  ADD PRIMARY KEY (`id_tarif`),
-  ADD UNIQUE KEY `id_RESERVA_UNIQUE` (`id_tarif`);
+ALTER TABLE `TARIFF`
+  ADD PRIMARY KEY (`id_tariff`),
+  ADD UNIQUE KEY `id_tariff_UNIQUE` (`id_tariff`);
 
 --
 -- Indexes for table `TIPO_PAGO`
 --
-ALTER TABLE `TIPO_PAGO`
-  ADD PRIMARY KEY (`id_tipopago`),
-  ADD UNIQUE KEY `id_tipopago_UNIQUE` (`id_tipopago`),
-  ADD UNIQUE KEY `nom_tipopago_UNIQUE` (`nom_tipopago`);
+ALTER TABLE `PAYMENT_TYPE`
+  ADD PRIMARY KEY (`id_paytype`),
+  ADD UNIQUE KEY `id_paytype_UNIQUE` (`id_paytype`),
+  ADD UNIQUE KEY `name_paytype_UNIQUE` (`name_paytype`);
 
 --
 -- Indexes for table `TIPO_RESERVA`
 --
-ALTER TABLE `TIPO_RESERVA`
-  ADD PRIMARY KEY (`id_tiporeserv`);
+ALTER TABLE `RESERVE_TYPE`
+  ADD PRIMARY KEY (`id_reservetype`);
 
 --
 -- Indexes for table `USUARIO`
 --
-ALTER TABLE `USUARIO`
-  ADD PRIMARY KEY (`rut_usu`),
-  ADD UNIQUE KEY `rut_EMPLEADO_UNIQUE` (`rut_usu`);
+ALTER TABLE `USER`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `id_user_UNIQUE` (`id_user`);
 
 --
 -- Indexes for table `USUARIO_AUTO`
 --
-ALTER TABLE `USUARIO_AUTO`
-  ADD PRIMARY KEY (`id_autousu`),
-  ADD KEY `fk_rut_usu_auto` (`rut_usu`),
-  ADD KEY `fk_pat_auto` (`pat_auto`);
+ALTER TABLE `USER_CAR`
+  ADD PRIMARY KEY (`id_usercar`),
+  ADD KEY `fk_id_user` (`id_user`),
+  ADD KEY `fk_id_car` (`id_car`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -332,68 +332,68 @@ ALTER TABLE `USUARIO_AUTO`
 --
 -- AUTO_INCREMENT for table `AUTO`
 --
-ALTER TABLE `AUTO`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `CAR`
+  MODIFY `id_brand` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `CODIGO_QR`
 --
-ALTER TABLE `CODIGO_QR`
-  MODIFY `id_codqr` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `QR_CODE`
+  MODIFY `id_qrcode` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `MARCA`
 --
-ALTER TABLE `MARCA`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `BRAND`
+  MODIFY `id_brand` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `PAGO`
 --
-ALTER TABLE `PAGO`
-  MODIFY `id_tipopago` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `PAYMENT`
+  MODIFY `id_paytype` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `PLAZA`
 --
-ALTER TABLE `PLAZA`
-  MODIFY `id_plaza` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `SEAT`
+  MODIFY `id_seat` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `RESERVA`
 --
-ALTER TABLE `RESERVA`
-  MODIFY `id_plaza` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `RESERVE`
+  MODIFY `id_seat` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `SECCION_PLAZA`
 --
-ALTER TABLE `SECCION_PLAZA`
-  MODIFY `id_seccionplaza` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `SEAT_SECTION`
+  MODIFY `id_seatsection` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `TARIFA`
 --
-ALTER TABLE `TARIFA`
-  MODIFY `id_tarif` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `TARIFF`
+  MODIFY `id_tariff` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `TIPO_PAGO`
 --
-ALTER TABLE `TIPO_PAGO`
-  MODIFY `id_tipopago` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `PAYMENT_TYPE`
+  MODIFY `id_paytype` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `TIPO_RESERVA`
 --
-ALTER TABLE `TIPO_RESERVA`
-  MODIFY `id_tiporeserv` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `RESERVE_TYPE`
+  MODIFY `id_reservetype` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `USUARIO_AUTO`
 --
-ALTER TABLE `USUARIO_AUTO`
-  MODIFY `id_autousu` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `USER_CAR`
+  MODIFY `id_usercar` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -402,40 +402,40 @@ ALTER TABLE `USUARIO_AUTO`
 --
 -- Constraints for table `AUTO`
 --
-ALTER TABLE `AUTO`
-  ADD CONSTRAINT `fk_id_marca` FOREIGN KEY (`id_marca`) REFERENCES `MARCA` (`id_marca`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_id_mod` FOREIGN KEY (`id_mod`) REFERENCES `MODELO_TIPO_AUTO` (`id_mod`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `CAR`
+  ADD CONSTRAINT `fk_id_brand` FOREIGN KEY (`id_brand`) REFERENCES `BRAND` (`id_brand`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_id_model` FOREIGN KEY (`id_model`) REFERENCES `MODEL` (`id_model`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `PAGO`
 --
-ALTER TABLE `PAGO`
-  ADD CONSTRAINT `fk_id_tipopago` FOREIGN KEY (`id_tipopago`) REFERENCES `TIPO_PAGO` (`id_tipopago`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `PAYMENT`
+  ADD CONSTRAINT `fk_id_paytype` FOREIGN KEY (`id_paytype`) REFERENCES `PAYMENT_TYPE` (`id_paytype`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `PLAZA`
 --
-ALTER TABLE `PLAZA`
-  ADD CONSTRAINT `fk_id_seccionplaza` FOREIGN KEY (`id_seccionplaza`) REFERENCES `SECCION_PLAZA` (`id_seccionplaza`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `SEAT`
+  ADD CONSTRAINT `fk_id_seatsection` FOREIGN KEY (`id_seatsection`) REFERENCES `SEAT_SECTION` (`id_seatsection`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `RESERVA`
 --
-ALTER TABLE `RESERVA`
-  ADD CONSTRAINT `fk_id_codqr` FOREIGN KEY (`id_codqr`) REFERENCES `CODIGO_QR` (`id_codqr`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_id_detserv` FOREIGN KEY (`id_detserv`) REFERENCES `DETALLE_SERVICIO` (`id_detserv`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_id_pago` FOREIGN KEY (`id_pago`) REFERENCES `PAGO` (`id_pago`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_id_plaza` FOREIGN KEY (`id_plaza`) REFERENCES `PLAZA` (`id_plaza`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_id_tarif` FOREIGN KEY (`id_tarif`) REFERENCES `TARIFA` (`id_tarif`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_id_tiporeserv` FOREIGN KEY (`id_tiporeserv`) REFERENCES `TIPO_RESERVA` (`id_tiporeserv`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_rut_usu` FOREIGN KEY (`rut_usu`) REFERENCES `USUARIO` (`rut_usu`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `RESERVE`
+  ADD CONSTRAINT `fk_id_qrcode` FOREIGN KEY (`id_qrcode`) REFERENCES `CODIGO_QR` (`id_qrcode`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_id_servdet` FOREIGN KEY (`id_servdet`) REFERENCES `SERVICE_DETAIL` (`id_servdet`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_id_payment` FOREIGN KEY (`id_payment`) REFERENCES `PAYMENT` (`id_payment`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_id_seat` FOREIGN KEY (`id_seat`) REFERENCES `SEAT` (`id_seat`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_id_tariff` FOREIGN KEY (`id_tariff`) REFERENCES `TARIFF` (`id_tariff`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_id_reservetype` FOREIGN KEY (`id_reservetype`) REFERENCES `RESERVE_TYPE` (`id_reservetype`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `USER` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `USUARIO_AUTO`
 --
-ALTER TABLE `USUARIO_AUTO`
-  ADD CONSTRAINT `fk_pat_auto` FOREIGN KEY (`pat_auto`) REFERENCES `AUTO` (`pat_auto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_rut_usu_auto` FOREIGN KEY (`rut_usu`) REFERENCES `USUARIO` (`rut_usu`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `USER_CAR`
+  ADD CONSTRAINT `fk_id_car` FOREIGN KEY (`id_car`) REFERENCES `CAR` (`id_car`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `USER` (`rut_usu`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
