ccfe2c32b5b0239ca0c27ce460db1465d4fe0ebd
ninelts <ckladiioxage@gmail.com>
2019-05-31 21:14:29 -0400

    Validaciones incompletas

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,12 +25,8 @@ class LoginController extends Controller
 
     public function authenticate(Request $request)
     {
-<<<<<<< HEAD
 
      if ( Auth::attempt(['rut_usu'=>$request->txt_rut,'password'=>$request->contr_usu])){
-=======
-     if ( Auth::attempt(['id_user'=>$request->txt_rut,'password'=>$request->contr_usu])){
->>>>>>> 0c14a6eaaf176f57a21b778cbc4ce0110e1a47e6
 
         // return response()->json( [ 'success' => true, 'message' => 'Acceso satisfactorio' ] );
 
@@ -62,13 +58,6 @@ use AuthenticatesUsers;
     {
         $this->middleware('guest')->except('logout');
     }
-<<<<<<< HEAD
 
-=======
-      public function username()
-    {
-        return 'id_user';
-    }
->>>>>>> 0c14a6eaaf176f57a21b778cbc4ce0110e1a47e6
 
 }
